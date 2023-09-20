7b5179aecca34cc89afff5899cb5740a546bc2c8
Rtao <prostodimka519@gmail.com>
2023-09-20 23:17:15 +0300

    telegram test

--- UI/telegram/TelegramButton.tsx
+++ UI/telegram/TelegramButton.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, Dispatch, SetStateAction } from 'react'
+/* import React, { useEffect, Dispatch, SetStateAction } from 'react'
 import TelegramLoginButton, { TelegramUser } from 'telegram-login-button'
-import styles from './telegramButton.module.css'
+
 interface IValueForButtons {
     telegram: boolean;
     wallet: boolean;
@@ -24,22 +24,56 @@ const TelegramButton: React.FC<TelegramButtonProps> = ({ setButtonArrValue }) =>
 
     useEffect(() => {
         setTimeout(() => {
-            const iframe: HTMLElement | null = document.querySelector("iframe")
+            const iframe = document.getElementById("telegram-login-RtaoTestTelegramBot")
             if (iframe) {
-                iframe.removeAttribute('style')
-                iframe.setAttribute("width", "170px")
-                iframe.setAttribute("height", "170px")
+
+                const iframeTag = document.querySelector<HTMLIFrameElement>('#telegram-login-RtaoTestTelegramBot') || null
+
+                if (iframeTag && iframeTag.contentWindow) {
+                    const elmnt = iframeTag.contentWindow.document.getElementsByTagName("body")[0]
+                    elmnt.style.height = "800px"
+                    elmnt.style.width = "800px"
+                    elmnt.style.backgroundColor = "red"
+                    console.log(iframeTag, elmnt)
+                }
             }
-            const teleg = document.getElementsByClassName("tgme_widget_login.small")
-            console.log(teleg)
         }, 1000)
     }, [])
 
     return (
-        <div>
-            <Telegram />
-        </div>
+        <Telegram />
     )
+}
+
+export default TelegramButton */
+
+
+
+
+
+import React from 'react'
+import styles from './telegramButton.module.css'
+import imgButtonTelegram from '../../pages/screenshots/1920-1680px/telegram.png'
+
+const TelegramButton = () => {
+    const handleLogin = () => {
+        const width = 400;
+        const height = 600;
+        const left = (window.innerWidth / 2) - (width / 2);
+        const top = (window.innerHeight / 2) - (height / 2);
+
+        window.open(
+            'https://oauth.telegram.org/auth?bot_id=6626043922&origin=https://rock-paper-scissors-navy-eta.vercel.app/&request_access=write',
+            'TelegramAuth',
+            `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width=${width}, height=${height}, top=${top}, left=${left}`
+        );
+    };
+
+    return (
+        <button onClick={handleLogin} className={styles.telegramLoginButton}>
+            <img src={imgButtonTelegram.src} alt="" />
+        </button>
+    );
 }
 
 export default TelegramButton
